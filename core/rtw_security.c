3a7a5bb7b9f22786963236190b73ae0b1346a515
Stephen Oliver <steve@infincia.com>
2020-12-14 22:17:21 -0500

    Avoid aliasing a few more sha256 symbols

--- core/rtw_security.c
+++ core/rtw_security.c
@@ -2130,7 +2130,7 @@ BIP_exit:
 
 #ifndef PLATFORM_FREEBSD
 /* compress 512-bits */
-static int sha256_compress(struct rtw_sha256_state *md, unsigned char *buf)
+static int rtw_sha256_compress(struct rtw_sha256_state *md, unsigned char *buf)
 {
 	u32 S[8], W[64], t0, t1;
 	u32 t;
@@ -2178,7 +2178,7 @@ static int sha256_compress(struct rtw_sha256_state *md, unsigned char *buf)
 }
 
 /* Initialize the hash state */
-static void sha256_init(struct rtw_sha256_state *md)
+static void rtw_sha256_init(struct rtw_sha256_state *md)
 {
 	md->curlen = 0;
 	md->length = 0;
@@ -2199,7 +2199,7 @@ static void sha256_init(struct rtw_sha256_state *md)
    @param inlen  The length of the data (octets)
    @return CRYPT_OK if successful
 */
-static int sha256_process(struct rtw_sha256_state *md, unsigned char *in,
+static int rtw_sha256_process(struct rtw_sha256_state *md, unsigned char *in,
 			  unsigned long inlen)
 {
 	unsigned long n;
@@ -2210,7 +2210,7 @@ static int sha256_process(struct rtw_sha256_state *md, unsigned char *in,
 
 	while (inlen > 0) {
 		if (md->curlen == 0 && inlen >= block_size) {
-			if (sha256_compress(md, (unsigned char *) in) < 0)
+			if (rtw_sha256_compress(md, (unsigned char *) in) < 0)
 				return -1;
 			md->length += block_size * 8;
 			in += block_size;
@@ -2222,7 +2222,7 @@ static int sha256_process(struct rtw_sha256_state *md, unsigned char *in,
 			in += n;
 			inlen -= n;
 			if (md->curlen == block_size) {
-				if (sha256_compress(md, md->buf) < 0)
+				if (rtw_sha256_compress(md, md->buf) < 0)
 					return -1;
 				md->length += 8 * block_size;
 				md->curlen = 0;
@@ -2240,7 +2240,7 @@ static int sha256_process(struct rtw_sha256_state *md, unsigned char *in,
    @param out [out] The destination of the hash (32 bytes)
    @return CRYPT_OK if successful
 */
-static int sha256_done(struct rtw_sha256_state *md, unsigned char *out)
+static int rtw_sha256_done(struct rtw_sha256_state *md, unsigned char *out)
 {
 	int i;
 
@@ -2260,7 +2260,7 @@ static int sha256_done(struct rtw_sha256_state *md, unsigned char *out)
 	if (md->curlen > 56) {
 		while (md->curlen < 64)
 			md->buf[md->curlen++] = (unsigned char) 0;
-		sha256_compress(md, md->buf);
+		rtw_sha256_compress(md, md->buf);
 		md->curlen = 0;
 	}
 
@@ -2270,7 +2270,7 @@ static int sha256_done(struct rtw_sha256_state *md, unsigned char *out)
 
 	/* store length */
 	WPA_PUT_BE64(md->buf + 56, md->length);
-	sha256_compress(md, md->buf);
+	rtw_sha256_compress(md, md->buf);
 
 	/* copy output */
 	for (i = 0; i < 8; i++)
@@ -2287,17 +2287,17 @@ static int sha256_done(struct rtw_sha256_state *md, unsigned char *out)
  * @mac: Buffer for the hash
  * Returns: 0 on success, -1 of failure
  */
-static int sha256_vector(size_t num_elem, u8 *addr[], size_t *len,
+static int rtw_sha256_vector(size_t num_elem, u8 *addr[], size_t *len,
 			 u8 *mac)
 {
 	struct rtw_sha256_state ctx;
 	size_t i;
 
-	sha256_init(&ctx);
+	rtw_sha256_init(&ctx);
 	for (i = 0; i < num_elem; i++)
-		if (sha256_process(&ctx, addr[i], len[i]))
+		if (rtw_sha256_process(&ctx, addr[i], len[i]))
 			return -1;
-	if (sha256_done(&ctx, mac))
+	if (rtw_sha256_done(&ctx, mac))
 		return -1;
 	return 0;
 }
@@ -2355,7 +2355,7 @@ static void hmac_sha256_vector(u8 *key, size_t key_len, size_t num_elem,
 
 	/* if key is longer than 64 bytes reset it to key = SHA256(key) */
 	if (key_len > 64) {
-		sha256_vector(1, &key, &key_len, tk);
+		rtw_sha256_vector(1, &key, &key_len, tk);
 		key = tk;
 		key_len = 32;
 	}
@@ -2383,7 +2383,7 @@ static void hmac_sha256_vector(u8 *key, size_t key_len, size_t num_elem,
 		_addr[i + 1] = addr[i];
 		_len[i + 1] = len[i];
 	}
-	sha256_vector(1 + num_elem, _addr, _len, mac);
+	rtw_sha256_vector(1 + num_elem, _addr, _len, mac);
 
 	_rtw_memset(k_pad, 0, sizeof(k_pad));
 	_rtw_memcpy(k_pad, key, key_len);
@@ -2396,7 +2396,7 @@ static void hmac_sha256_vector(u8 *key, size_t key_len, size_t num_elem,
 	_len[0] = 64;
 	_addr[1] = mac;
 	_len[1] = 32;
-	sha256_vector(2, _addr, _len, mac);
+	rtw_sha256_vector(2, _addr, _len, mac);
 }
 #endif /* PLATFORM_FREEBSD */
 /**
@@ -2413,7 +2413,7 @@ static void hmac_sha256_vector(u8 *key, size_t key_len, size_t num_elem,
  * given key.
  */
 #ifndef PLATFORM_FREEBSD /* Baron */
-static void sha256_prf(u8 *key, size_t key_len, char *label,
+static void rtw_sha256_prf(u8 *key, size_t key_len, char *label,
 		       u8 *data, size_t data_len, u8 *buf, size_t buf_len)
 {
 	u16 counter = 1;
@@ -2437,10 +2437,10 @@ static void sha256_prf(u8 *key, size_t key_len, char *label,
 	while (pos < buf_len) {
 		plen = buf_len - pos;
 		WPA_PUT_LE16(counter_le, counter);
-		if (plen >= SHA256_MAC_LEN) {
+		if (plen >= RTW_SHA256_MAC_LEN) {
 			hmac_sha256_vector(key, key_len, 4, addr, len,
 					   &buf[pos]);
-			pos += SHA256_MAC_LEN;
+			pos += RTW_SHA256_MAC_LEN;
 		} else {
 			hmac_sha256_vector(key, key_len, 4, addr, len, hash);
 			_rtw_memcpy(&buf[pos], hash, plen);
@@ -2873,7 +2873,7 @@ void wpa_tdls_generate_tpk(_adapter *padapter, PVOID sta)
 		nonce[1] = SNonce;
 	}
 
-	sha256_vector(2, nonce, len, key_input);
+	rtw_sha256_vector(2, nonce, len, key_input);
 
 	/*
 	 * TPK-Key-Data = KDF-N_KEY(TPK-Key-Input, "TDLS PMK",
@@ -2892,7 +2892,7 @@ void wpa_tdls_generate_tpk(_adapter *padapter, PVOID sta)
 	}
 	_rtw_memcpy(data + 2 * ETH_ALEN, get_bssid(pmlmepriv), ETH_ALEN);
 
-	sha256_prf(key_input, SHA256_MAC_LEN, "TDLS PMK", data, sizeof(data), (u8 *) &psta->tpk, sizeof(psta->tpk));
+	rtw_sha256_prf(key_input, RTW_SHA256_MAC_LEN, "TDLS PMK", data, sizeof(data), (u8 *) &psta->tpk, sizeof(psta->tpk));
 
 
 }
